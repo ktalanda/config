757c70eb0220c9f0918793a53bf23feb99b1eb6a
kamil <kamil0916@gmail.com>
2018-12-21 17:40:20 +1100

    Initial commit.

---
+++ config.sh
@@ -0,0 +1,9 @@
+mv ~/.zshrc ~/tmp/.zshrc
+ln -s ~/config/.zshrc ~/.zshrc
+
+mv ~/.vimrc ~/tmp/.vimrc
+ln -s ~/config/.vimrc ~/.vimrc
+
+mv ~/.gitconfig ~/tmp/.gitconfig
+ln -s ~/config/.gitconfig ~/.gitconfig
+
