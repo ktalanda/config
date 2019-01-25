14fdcdb9619cc8007bc052ea4b144d645382e34f
Kamil Talanda <kamil0916@gmail.com>
2019-01-26 10:31:29 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,31 @@
-brew install git
-brew install nvm
-brew cask install slack
-brew cask install android-sdk
-brew cask install iterm2
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-brew install zsh-syntax-highlighting
-source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew install git
+
+brew cask install iterm2
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+brew install thefuck
+brew install zsh-syntax-highlighting
+brew install zsh-autosuggestions
+# powerline/fonts
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
+# change iTerm2->Preferences->Profiles->Text->Font to Meslo
+# change iTerm2->Preferences->Profiles->Colors->ANSI Colors->Bright
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+chmod +x ./config/config.sh
+./config/config.sh
+
+brew cask install slack
+# snappy - AppStore
+brew cask install kap
+
+brew install nvm
 
 # magnet
-# kap
-# snappy
 # one-password
 # flux
 # xcode
